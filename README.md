dcead928a6f5d28679e968797a2ebc57a19c8e9b
Tristan Farkas <tristan@tristanfarkas.se>
2021-11-07 03:31:09 +0100

    add mac support, very hacky 3am fix dont actually look at it thanks

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Rift_Insight
 A quick and flexible API binding for the League of Legends [LCU API](https://technology.riotgames.com/news/architecture-league-client-update) and also includes support for the in game API.  
-Windows only support right now.  
+
 
 Rift_Insight was created under Riot Games' ["Legal Jibber Jabber"](https://www.riotgames.com/en/legal) policy using assets owned by Riot Games.  Riot Games does not endorse or sponsor this project.
--- src/locate/lock_file.rs
+++ src/locate/lock_file.rs
@@ -3,6 +3,14 @@ use crate::errors::{LockFileError, RiftInitializationError, LockfileErrorTypes, 
 use std::fs::read_to_string;
 use std::process::{Command, Stdio};
 
+use regex::Regex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref REMOTING_AUTH_TOKEN: Regex = Regex::new("--remoting-auth-token=([\\w-]*)").unwrap();
+    static ref APP_PORT: Regex = Regex::new("--app-port=([0-9]*)").unwrap();
+}
+
 #[derive(Debug, Clone)]
 pub struct LockfileContents {
     pub port: u16,
@@ -52,7 +60,42 @@ pub fn get_lockfile() -> Result<LockfileContents, RiftInitializationError> {
             Err(e) => { Err(e) }
         }
     } else {
-        return Err(RiftInitializationError::UnknownError("Unsupported platform.".to_string()))
+        if cfg!(target_os = "macos") {
+            let out = Command::new("ps")
+                .args(["-A"])
+                .stdout(Stdio::piped())
+                .output()
+                .unwrap();
+            let out_string = String::from_utf8(out.stdout).unwrap();
+            let mut stdout = out_string.lines()
+                .filter(|line| line.find("LeagueClientUx").is_some());
+            let first_line = stdout.next().unwrap_or("");
+            if !first_line.contains("LeagueClientUx") {
+                return Err(RiftInitializationError::GenericLockfile(LockFileError::new(LockfileErrorTypes::LeagueNotRunning)));
+            }
+            match REMOTING_AUTH_TOKEN.find(first_line) {
+                Some(remote_auth) => {
+                    match APP_PORT.find(first_line) {
+                        Some(port) => {
+                            return Ok(LockfileContents {
+                                
+                                port: port.as_str().split("=").collect::<Vec<&str>>()[1].parse::<u16>().unwrap(),
+                                password: remote_auth.as_str().split("=").collect::<Vec<&str>>()[1].to_string(),
+                                protocol: "https".to_string()
+                            })
+                        }
+                        None => {
+                            return Err(RiftInitializationError::GenericLockfile(LockFileError::new(LockfileErrorTypes::LeagueNotRunning)));
+                        }
+                    }
+                },
+                None => {
+                    return Err(RiftInitializationError::GenericLockfile(LockFileError::new(LockfileErrorTypes::LeagueNotRunning)));
+                }
+            }
+        } else {
+            return Err(RiftInitializationError::UnknownError("Unsupported platform.".to_string()))
+        }
     }
     
 }
